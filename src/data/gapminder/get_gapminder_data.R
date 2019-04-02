4bdac1151b0b1400e56fb3b45bd379e77e55ca00
should16$ <stephen@nnepra.com>
2019-04-02 14:46:33 +0000

    get_gapminder_data.R

---
+++ src/data/gapminder/get_gapminder_data.R
@@ -0,0 +1,7 @@
+# Download Gapminder data for analysis
+#
+#
+library(here)
+
+file_url <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv"
+
